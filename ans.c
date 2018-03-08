38293897c4faa95021054b5cdef7f4770de233ac
loveehingorani <32771261+loveehingorani@users.noreply.github.com>
2018-03-09 00:03:51 +0530

    Add files via upload

---
+++ ans.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+int i,j,x=1,n;
+
+
+printf("enter the value of n");
+scanf("%d",&n);
+
+
+for(i=1;i<=n;i++)
+
+{printf("\n");
+
+  for(j=1;j<=i;j++)
+  {
+    printf("%d",x++);
+
+  }
+}
+
+}
